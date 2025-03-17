33e5202c4b689ce67275d646b83de42efce7c4e2
Maxim Smirnou <cvmaksim@gmail.com>
2025-03-17 13:30:19 +0300

    Added guards for user authentication and roles auth

---
+++ api-gateway/src/auth/auth.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(@Inject('AUTH_SERVICE') private authServiceClient: ClientProxy) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload: Record<number, string> = await firstValueFrom(
+        this.authServiceClient.send('verifyToken', token),
+      );
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
