919064b1636516a48f39e80f78c3ee2ee3c11ea1
Maxim Smirnou <cvmaksim@gmail.com>
2025-03-18 12:45:40 +0300

    Naming change

--- auth_service/src/config/jwt_config_service.ts
+++ auth_service/src/config/jwt_config_service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthConfigService implements JwtOptionsFactory {
+export class JwtConfigService implements JwtOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
   createJwtOptions(): JwtModuleOptions {
     return {
